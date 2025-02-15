5f03634c3113fd3afd169173e89e72936b226dad
Ferrian Septiawan <ferrian27@gmail.com>
2025-02-15 12:20:24 +0700

    feat: remove App component and integrate registration page with routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { Register } from './pages/auth/register/page'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route index element={<Register />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
